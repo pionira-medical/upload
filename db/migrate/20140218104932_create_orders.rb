85022cbbefb507171d092669fba38a9a465f4fc0
Gerrit Wanderer <gerrit@the-wanderer.de>
2014-02-18 11:49:53 +0100

    Add Order Model

---
+++ db/migrate/20140218104932_create_orders.rb
@@ -0,0 +1,11 @@
+class CreateOrders < ActiveRecord::Migration
+  def change
+    create_table :orders do |t|
+      t.text :description
+      t.integer :order_number
+      t.reference :admin_user
+
+      t.timestamps
+    end
+  end
+end
