8f12dd903f1e5e04b4c378f0f50d15263e7b4959
Gerrit Wanderer <gerrit@the-wanderer.de>
2014-02-21 11:51:33 +0100

    Point user login to root url

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :trackable, :validatable, :authentication_keys => [:id]
+  devise :database_authenticatable, :trackable, :validatable, authentication_keys: [:id]
 
   belongs_to :order
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '/', path_names: { sign_in: '' }
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # The priority is based upon order of creation: first created -> highest priority.
